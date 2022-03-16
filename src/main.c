af404f2d03aed34289e71db0b79ef9132fc2aa1b
Moarbue <tomkaufmann1206@gmail.com>
2022-03-16 21:34:24 +0100

    Added main loop (exchange copied word with matching word if existing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "file_helper.h"
+#include "libclipboard.h"
 
 int main()
 {
@@ -22,6 +24,50 @@ int main()
 
     res = splitData(fileContent, length, ",", "\n", &pairs, &paircnt);
     assert(res == 0);
+
+
+    clipboard_c *cb = clipboard_new(NULL);
+    char *last_cb_content = NULL;
+
+    while(1)
+    {
+        #ifndef _WIN32
+        #  include <unistd.h>
+            sleep(1);
+        #else
+        #  include <windows.h>
+            Sleep(1000);
+        #endif
+
+        char *cb_content = clipboard_text(cb);
+        if (cb_content == NULL || cb_content == last_cb_content) continue;
+
+        size_t i; bool reverse = false;
+        for (i = 0; i < paircnt; i++)
+        {
+            if (strstr(pairs[i].first, cb_content) != NULL) break;
+        }
+
+        if (i == paircnt)
+        {
+            for (i = 0; i < paircnt; i++)
+            {
+                if (strstr(pairs[i].second, cb_content) != NULL) break;
+            }
+            reverse = true;
+        }
+
+        if (i == paircnt)
+            clipboard_set_text(cb, "none");
+        else if (reverse)
+            clipboard_set_text(cb, pairs[i].first);
+        else
+            clipboard_set_text(cb, pairs[i].second);
+
+        last_cb_content = realloc(last_cb_content, strlen(cb_content));
+        strcpy(last_cb_content, cb_content);
+    }
+
 
     free(pairs);
     return 0;
