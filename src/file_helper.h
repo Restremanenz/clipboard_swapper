ed1fd4a8b9ee9f7b48d3f91c730e7a1b20ec8067
Moarbue <tomkaufmann1206@gmail.com>
2022-03-16 19:25:50 +0100

    Added file helper source/header file for reading whole content of file at once

---
+++ src/file_helper.h
@@ -0,0 +1,29 @@
+#ifndef FILE_HELPER_H
+#define FILE_HELPER_H
+
+#include <stdio.h>
+
+/* Size of each input chunk to be
+   read and allocate for. */
+#ifndef  READALL_CHUNK
+#define  READALL_CHUNK  262144
+#endif
+
+#define  READALL_OK          0  /* Success */
+#define  READALL_INVALID    -1  /* Invalid parameters */
+#define  READALL_ERROR      -2  /* Stream error */
+#define  READALL_TOOMUCH    -3  /* Too much input */
+#define  READALL_NOMEM      -4  /* Out of memory */
+
+/* This function returns one of the READALL_ constants above.
+   If the return value is zero == READALL_OK, then:
+     (*dataptr) points to a dynamically allocated buffer, with
+     (*sizeptr) chars read from the file.
+     The buffer is allocated for one extra char, which is NUL,
+     and automatically appended after the data.
+   Initial values of (*dataptr) and (*sizeptr) are ignored.
+*/
+int readFile(FILE *in, char **dataptr, size_t *sizeptr);
+
+
+#endif // FILE_HELPER_H
