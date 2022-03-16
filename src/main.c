2072dcf21e8c413bedd3ab066c12d282aae64dc1
Moarbue <tomkaufmann1206@gmail.com>
2022-03-16 19:27:12 +0100

    Added assert statements

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 #include "file_helper.h"
 
 int main()
@@ -7,8 +8,10 @@ int main()
     size_t length = 0;
 
     FILE* f = fopen("./wordTable.txt", "rb");
+    assert(f);
     
     int res = readFile(f, &fileContent, &length);
+    assert(res == 0);
 
     printf("%s\n", fileContent);
 
