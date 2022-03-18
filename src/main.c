91cbde2e0a90d5c320d3198ad57719af0863e25d
Moarbue <tomkaufmann1206@gmail.com>
2022-03-18 19:04:37 +0100

    Deleted main loop, needs new implementation

--- src/main.c
+++ src/main.c
@@ -39,8 +39,10 @@ int main()
 
     // create clipboard object
     clipboard_c *cb = clipboard_new(NULL);
+    
+    char *cb_content = clipboard_text(cb);
     // clipboard content of last iteration
-    char *last_cb_content = NULL;
+    char *last_cb_content = cb_content;
 
     while(1)
     {
@@ -50,42 +52,6 @@ int main()
         #else
             Sleep(1000);
         #endif
-
-        // read clipboard content into string
-        char *cb_content = clipboard_text(cb);
-        // check if clipboard content is NULL or changed since last iteration
-        if (cb_content == NULL || cb_content == last_cb_content) continue;
-
-        // check if clipboard content matches a word of the first string of the pairs
-        size_t i; bool reverse = false;
-        for (i = 0; i < paircnt; i++)
-        {
-            if (strstr(pairs[i].first, cb_content) != NULL) break;
-        }
-
-        // didn't found any matching words
-        if (i == paircnt)
-        {
-            // check if clipboard content matches a word of the second string of the pairs
-            for (i = 0; i < paircnt; i++)
-            {
-                if (strstr(pairs[i].second, cb_content) != NULL) break;
-            }
-            reverse = true;
-        }
-        // didn't found any matching words
-        if (i == paircnt)
-            clipboard_set_text(cb, "none");
-        // found matching word in the second string of the pairs
-        else if (reverse)
-            clipboard_set_text(cb, pairs[i].first);
-        // found matching word in the first string of the pairs
-        else
-            clipboard_set_text(cb, pairs[i].second);
-
-        // update the last clipboard content variable
-        last_cb_content = realloc(last_cb_content, strlen(cb_content));
-        strcpy(last_cb_content, cb_content);
     }
 
     // free clipboard and other pointers
